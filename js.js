9abfdc2d5c437b071a6fe2c28698c49ee07ea4d3
alexghb <qaz1wsx2>
2015-12-28 10:15:38 +0200

    js

--- js.js
+++ js.js
@@ -2,3 +2,15 @@ function imageIsLoaded(img) {
     if (!img.complete) return false;
     return !(typeof img.naturalWidth != 'undefined' && img.naturalWidth == 0);
 }
+
+function setUserAgent(w, userAgent) {
+    if (w.navigator.userAgent != userAgent) {
+        var userAgentProp = { get: function () { return userAgent; } };
+        try {
+            Object.defineProperty(w.navigator, 'userAgent', userAgentProp);
+        } catch (e) {
+            w.navigator = Object.create(navigator, { userAgent: userAgentProp });
+        }
+    }
+}
+setUserAgent(document.querySelector('iframe').contentWindow, 'A User Agent String');
